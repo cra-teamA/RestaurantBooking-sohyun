811bc666eaf0bf9b649e8848c997291e5b17a032
soiscow <thgus_so@naver.com>
2025-08-01 12:49:41 +0900

    [refact] test refactoring

--- test_booking_schedule.py
+++ test_booking_schedule.py
@@ -1,46 +1,61 @@
 import pytest
 
 from schedule import Customer, Schedule
+
+UNDER_CAPACITY = 1
 from communication import SmsSender, MailSender
 from booking_scheduler import BookingScheduler
 from datetime import datetime
 
-def test_예약은_정시에만_가능하다_정시가_아닌경우_예약불가():
-    not_on_the_hour = datetime.strptime("2025/08/01 12:36", "%Y/%m/%d %H:%M")
-    customer = Customer("so", "010-1234-5678")
-    schedule = Schedule(not_on_the_hour, 1, customer)
-    booking_scheduler = BookingScheduler(3)
+NOT_ON_THE_HOUR = datetime.strptime("2025/08/01 12:36", "%Y/%m/%d %H:%M")
+CUSTOMER = Customer("so", "010-1234-5678")
+ON_THE_HOUR = datetime.strptime("2025/08/01 12:00", "%Y/%m/%d %H:%M")
+
+UNDER_CAPACITY = 1
+CAPACITY_PER_HOUR = 3
+
+@pytest.fixture()
+def booking_scheduler():
+    return BookingScheduler(CAPACITY_PER_HOUR)
+
+def test_예약은_정시에만_가능하다_정시가_아닌경우_예약불가(booking_scheduler):
+    schedule = Schedule(NOT_ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
 
     with pytest.raises(ValueError):
         booking_scheduler.add_schedule(schedule)
 
-def test_예약은_정시에만_가능하다_정시인_경우_예약가능():
-    not_on_the_hour = datetime.strptime("2025/08/01 12:00", "%Y/%m/%d %H:%M")
-    customer = Customer("so", "010-1234-5678")
-    schedule = Schedule(not_on_the_hour, 1, customer)
-    booking_scheduler = BookingScheduler(3)
+
+def test_예약은_정시에만_가능하다_정시인_경우_예약가능(booking_scheduler):
+    schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
 
     booking_scheduler.add_schedule(schedule)
 
     assert booking_scheduler.has_schedule(schedule)
 
+
 def test_시간대별_인원제한이_있다_같은_시간대에_Capacity_초과할_경우_예외발생():
     pass
+
 
 def test_시간대별_인원제한이_있다_같은_시간대가_다르면_Capacity_차있어도_스케쥴_추가_성공():
     pass
 
+
 def test_예약완료시_SMS는_무조건_발송():
     pass
+
 
 def test_이메일이_없는_경우에는_이메일_미발송():
     pass
 
+
 def test_이메일이_있는_경우에는_이메일_발송():
     pass
 
+
 def test_현재날짜가_일요일인_경우_예약불가_예외처리():
     pass
+
 
 def test_현재날짜가_일요일이_아닌경우_예약가능():
     pass
