2ab34fb0f9fa7ef8c671fd800c1f69a6bbda483f
soiscow <thgus_so@naver.com>
2025-08-01 12:53:58 +0900

    [feat] add upper-capa error test

--- test_booking_schedule.py
+++ test_booking_schedule.py
@@ -14,9 +14,11 @@ ON_THE_HOUR = datetime.strptime("2025/08/01 12:00", "%Y/%m/%d %H:%M")
 UNDER_CAPACITY = 1
 CAPACITY_PER_HOUR = 3
 
+
 @pytest.fixture()
 def booking_scheduler():
     return BookingScheduler(CAPACITY_PER_HOUR)
+
 
 def test_예약은_정시에만_가능하다_정시가_아닌경우_예약불가(booking_scheduler):
     schedule = Schedule(NOT_ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
@@ -33,8 +35,14 @@ def test_예약은_정시에만_가능하다_정시인_경우_예약가능(booki
     assert booking_scheduler.has_schedule(schedule)
 
 
-def test_시간대별_인원제한이_있다_같은_시간대에_Capacity_초과할_경우_예외발생():
-    pass
+def test_시간대별_인원제한이_있다_같은_시간대에_Capacity_초과할_경우_예외발생(booking_scheduler):
+    schedule = Schedule(ON_THE_HOUR, CAPACITY_PER_HOUR, CUSTOMER)
+
+    booking_scheduler.add_schedule(schedule)
+
+    with pytest.raises(ValueError, match="Number of people is over restaurant capacity per hour"):
+        new_schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
+        booking_scheduler.add_schedule(new_schedule)
 
 
 def test_시간대별_인원제한이_있다_같은_시간대가_다르면_Capacity_차있어도_스케쥴_추가_성공():
